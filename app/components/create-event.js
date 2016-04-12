8e7c1648ff7af2a9c99cc9ee7980a706e15e17e8
Erik Tolentino and Jess Guilford <jess.guilford@gmail.com>
2016-04-12 16:10:50 -0700

    add bookmark service and route

--- app/components/create-event.js
+++ app/components/create-event.js
@@ -3,12 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     saveEvent(){
+      var params = {
       name: this.get('name'),
       date: this.get('date'),
       location: this.get('location'),
       description: this.get('description'),
       image: this.get('image')
-    };
-    this.send
+      };
+      this.sendAction('saveNewEvent', params);
+    }
   }
 });
