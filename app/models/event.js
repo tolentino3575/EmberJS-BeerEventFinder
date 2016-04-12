08b41f5a8c383a283fa3895f12433c4da2ce6c90
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-04-12 13:54:43 -0700

    Add create-event form

--- app/models/event.js
+++ app/models/event.js
@@ -3,7 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   date: DS.attr(),
-  location: DS.attr(),
+  address: DS.attr(),
+  zip: DS.attr(),
   description: DS.attr(),
   image: DS.attr()
 });
