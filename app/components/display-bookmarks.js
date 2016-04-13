0ee8484207d08a2813b5eb3afc057da8a94bf7fb
Erik Tolentino and Jess Guilford <tolentino3575@gmail.com>
2016-04-13 16:02:19 -0700

    adjust sortby, add remove bookmark functionality

--- app/components/display-bookmarks.js
+++ app/components/display-bookmarks.js
@@ -2,4 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   bookmarks: Ember.inject.service(),
+  actions: {
+    removeFromBookmarks(event) {
+      if(confirm("Remove this event from your bookmarks?")) {
+        this.get('bookmarks').remove(event);
+      }
+    }
+  }
 });
