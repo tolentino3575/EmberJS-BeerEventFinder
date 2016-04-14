ae65ba872742816534236d4aebf05de26dfacb19
Erik Tolentino and Jess Guilford <tolentino3575@gmail.com>
2016-04-14 13:37:28 -0700

    add sticky header and more awesome styling

---
+++ app/components/sticky-header.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+const { $, run, computed } = Ember;
+
+export default Ember.Component.extend({
+  classNames:['sticky-header-container'],
+  classNameBindings: ['isSticky'],
+  topPos: 0,
+  isSticky: false,
+
+  onWindowScroll(e) {
+		let scroll = $(e.currentTarget).scrollTop();
+    let topPos = 0;
+    if (scroll > topPos) {
+    	this.set('isSticky', true);
+    } else {
+    	this.set('isSticky', false);
+    }
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._windowScroll = run.bind(this, 'onWindowScroll');
+    $(window).on('scroll', this._windowScroll);
+    //this.set('topPos', this.$('.sticky-header').offset().top);
+  },
+
+  willRemoveElement() {
+ 		$(window).off('scroll', this._windowScroll);
+  	this._super(...arguments);
+  }
+
+});
